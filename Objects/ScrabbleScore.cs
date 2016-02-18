dc2425a536855a921d90b27b8875af4e6fb882f8
simon chris <simonctemple@live.com>
2016-02-18 09:46:06 -0800

    add passing tests and functionality for getResults method

--- Objects/ScrabbleScore.cs
+++ Objects/ScrabbleScore.cs
@@ -13,10 +13,47 @@ namespace ScrabbleScoreNS.Objects
       _input = myInput;
     }
 
-    public GetResults()
+    public int GetResults()
     {
+      int points = 0;
       char[] inputArray = _input.ToLower().ToCharArray();
-      string results = string.Join("", inputArray);
+      for(var i = 0; i < inputArray.Length; i++)
+      {
+        if(inputArray[i] == 'a' || inputArray[i] == 'e' || inputArray[i] == 'i' || inputArray[i] == 'o' || inputArray[i] == 'u' || inputArray[i] == 'l' || inputArray[i] == 'n' || inputArray[i] == 'r' || inputArray[i] == 's' || inputArray[i] == 't')
+        {
+          points += 1;
+        }
+        else if (inputArray[i] == 'd' || inputArray[i] == 'g')
+        {
+          points += 2;
+        }
+        else if (inputArray[i] == 'b' || inputArray[i] == 'c' || inputArray[i] == 'm' || inputArray[i] == 'p')
+        {
+          points += 3;
+        }
+        else if (inputArray[i] == 'f' || inputArray[i] == 'h' || inputArray[i] == 'v' || inputArray[i] == 'w' || inputArray[i] == 'y')
+        {
+          points += 4;
+        }
+        else if (inputArray[i] == 'k')
+        {
+          points += 5;
+        }
+        else if (inputArray[i] == 'j' || inputArray[i] == 'x')
+        {
+          points += 8;
+        }
+        else if (inputArray[i] == 'q' || inputArray[i] == 'z')
+        {
+          points += 10;
+        }
+        else
+        {
+          points += 0;
+        }
+      }
+
+      return points;
     }
   }
 }
--- Tests/ScrabbleScoreTests.cs
+++ Tests/ScrabbleScoreTests.cs
@@ -6,17 +6,44 @@ namespace ScrabbleScoreNS.Objects
   public class ScrabbleScoreTest
   {
     [Fact]
-    public void Test1_ToChangeToLowerCase_ReturnLowerCase()
+    public void Test1_ToSeeIfPointsAdded_Return1()
     {
       //Arrange
-      string input1 = "HELLO";
-      ScrabbleScore testWord = new ScrabbleScore(input1);
+      string input1 = "A";
+      ScrabbleScore testPoints = new ScrabbleScore(input1);
 
       //Act
-      string result = testWord.GetResults();
+      int result = testPoints.GetResults();
 
       //Assert
-      Assert.Equal("hello", result);
+      Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Test2_ToSeeIfPointsAdded_ReturnDictionary()
+    {
+      //Arrange
+      string input1 = "A";
+      ScrabbleScore testPoints = new ScrabbleScore(input1);
+
+      //Act
+      int result = testPoints.GetResults();
+
+      //Assert
+      Assert.Equal(1, result);
+    }
+    [Fact]
+    public void Test3_ToSeeIfPointsAddedHell0_Return7()
+    {
+      //Arrange
+      string input1 = "hell0";
+      ScrabbleScore testPoints = new ScrabbleScore(input1);
+
+      //Act
+      int result = testPoints.GetResults();
+
+      //Assert
+      Assert.Equal(7, result);
     }
   }
 }
